3208c4d22b66b05eb0c941708fb1a16e59719d84
EricShi1255 <eshi20@stuy.edu>
2020-11-22 14:55:33 -0500

    testing

--- Tester.java
+++ Tester.java
@@ -7,7 +7,15 @@ public class Tester {
         System.out.println("15 & 15.00000001 --> true |" + a.equals(b));
         System.out.println("15 & -30 --> false |" + a.equals(c));
         System.out.println("15 & 16 --> false |" + a.equals(d));
-        RealNumber sumac = a.add(b);
-        System.out.println(sumac);
+        RealNumber sumac = a.add(c);
+        System.out.println("-15 | " + sumac.getValue());
+        RealNumber atimesc = a.multiply(c);
+        System.out.println("-450 |" + atimesc);
+        RealNumber cdividea = c.divide(a);
+        System.out.println("-2 |" + cdividea);
+
+        RationalNumber aa = (3,4);
+        System.out.println("3 | " + aa.getDenomminator());
+        System.out.println("4 | " + aa.getNumerator());
     }
 }
